5d0902cbfdfd8f813ff6f08b44f2330c73c72baf
Hai Kieu <haikieu2907@gmail.com>
2015-08-06 17:35:59 +0700

    Add project

---
+++ ConsumerProject/ConsumerProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConsumerProject
+//
+//  Created by HK on 8/6/15.
+//  Copyright (c) 2015 haikieu2907@icloud.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConsumerProject/ConsumerProject/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  ConsumerProject
+//
+//  Created by HK on 8/6/15.
+//  Copyright (c) 2015 haikieu2907@icloud.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
